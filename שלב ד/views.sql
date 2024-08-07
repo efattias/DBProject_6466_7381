bdd984f3b0833e0c82240c3b77b17020088d1de6
efattias <efattias@g.jct.ac.il>
2024-08-07 20:51:09 +0300

    tag 4

--- שלב ד/views.sql
+++ שלב ד/views.sql
@@ -4,18 +4,23 @@ FROM Event e
 JOIN MakeOrder mo ON e.eventID = mo.orderID
 JOIN Participants p ON mo.participantID = p.participantID;
 
+SELECT * FROM EventParticipants;
+
 SELECT firstName, lastName
 FROM EventParticipants
-WHERE eventID = 1;
+WHERE eventID > 100;
 
 SELECT eventName, eventDate
 FROM EventParticipants
-WHERE participantID = 1;
+WHERE participantID > 100;
 
 CREATE VIEW PaymentTaxDetails AS
 SELECT p.PAYMENT_ID, p.PAYMENT_AMOUNT, t.TAX_PRICE, t.TAX_CREATE
 FROM PAYMENT p
 JOIN TAX_ACCOUNT t ON p.TAX_ID = t.TAX_ID;
+
+SELECT * FROM PaymentTaxDetails;
+
 
 SELECT PAYMENT_ID, PAYMENT_AMOUNT
 FROM PaymentTaxDetails
@@ -23,7 +28,7 @@ WHERE PAYMENT_AMOUNT > 1000;
 
 SELECT TAX_PRICE, TAX_CREATE
 FROM PaymentTaxDetails
-WHERE PAYMENT_ID = 4855219992;
+WHERE PAYMENT_AMOUNT > 5000;
 
 
 --DROP VIEW EventParticipants;
